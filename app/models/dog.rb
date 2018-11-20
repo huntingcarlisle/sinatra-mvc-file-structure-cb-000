afec59bc36159987393eeb9d7dbf908d04662575
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-20 23:24:54 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,17 @@
 class Dog
   attr_accessor :name, :breed, :age
+  @@all = []
+  
   
   def initialize(name: name, breed: breed, age: age)
     @name = name
     @breed = breed
     @age = age
+    @@all << self
+  end
+  
+  def self.all
+    return @@all
   end
   
 end
